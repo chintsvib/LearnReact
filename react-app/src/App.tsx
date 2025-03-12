f3e6d43bbfe442046f64fae6df7f5fc0436d6a27
ChintanV <chintan.vibhakar@gmail.com>
2025-03-12 16:53:14 +0530

    Updating App

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 
@@ -10,16 +10,47 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    const controller = new AbortController();
+    setIsLoading(true);
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users', {signal: controller.signal})
       .then(response => setUsers(response.data))
-      .catch(error => console.error(error));
+      .catch(error => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+    return () => controller.abort();
   }, []);
 
-    return <ul>
-      {users.map(user => <li key={user.id}>{user.name}</li>)}
-    </ul>;
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter(u => u.id !== user.id));
+    axios.delete(`https://jsonplaceholder.typicode.com/xsers/${user.id}`)
+      .catch(error => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setUsers(originalUsers);
+      })
+  }
+
+  return (
+    <>
+      {isLoading && <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>}
+      {error && <p className="text-danger">{error}</p>}
+      {!isLoading && !error && <ul className="list-group">
+        {users.map(user => <li key={user.id} className="list-group-item d-flex justify-content-between">
+          {user.name}
+          <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+        </li>)}
+      </ul>}
+    </>
+  )
 }
 
 export default App;
